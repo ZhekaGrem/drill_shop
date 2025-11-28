e1f8432ec326897ecfb7b1b9551a9271c6100d06
Jeka <odinked@gmail.com>
2025-11-28 18:58:32 +0200

    fix seo2

--- src/app/seo.ts
+++ src/app/seo.ts
@@ -49,18 +49,13 @@ export const baseMetadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/favicon.svg', type: 'image/svg+xml' },
+      
       { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
       { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
       { url: '/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
     ],
     apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/favicon.svg',
-      },
-    ],
+    
   },
 
   manifest: '/manifest.json',
